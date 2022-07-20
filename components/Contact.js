1fab3fe8a12f68840c6da8b4a5da2724561890c7
fernando_n_dev <tierragraphics@gamil.com>
2022-07-20 12:00:24 -0700

    Widget & Contacts implemented

---
+++ components/Contact.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Contact({src, name}) {
+  return (
+    <div className="flex items-center space-x-3 mb-2 relative hover:bg-gray-200 cursor-pointer p-2">
+        <Image
+            className="rounded-full"
+            objectFit="cover"
+            src={src}
+            width={50}
+            height={50}
+            layout="fixed"
+        />
+        <p className="font-semibold">{name}</p>
+        <div className="absolute bottom-2 bg-green-400 h-3 w-3 rounded-full" />
+    </div>
+  )
+}
